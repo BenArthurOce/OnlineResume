d111c27deb4f8fc290e7e6b76b7fb0cedac4dbd6
Ben Arthur <benarthur@live.com>
2024-01-09 14:48:11 +1100

    adjusted landing page

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict"
+
+import ResumeData from "./libs/factoryResumeData.js";
+import HTMLskillTile from "./libs/HTMLskillTile.js";
+import HTMLeducation from "./libs/HTMLeducation.js";
+import HTMLportfolioTile from "./libs/HTMLportfolioTile.js";
+import HTMLexpTile from "./libs/HTMLexpTile.js";
+
+
+import HTMLSectionSlideshow  from './libs/HTMLsectionSideshow.js';
+
+
+const wrapperElement = document.getElementById('wrapper')
+const slideshowNEW = new HTMLSectionSlideshow(wrapperElement);
+
+
+
+const classResumeData = new ResumeData;
+
+// fetch json resume data
+classResumeData.getJSONdata()
+.then(resumeData => {
+
+    // Get the headers of the JSON file, and then render each section to the page
+    const {aboutMe, myCompetencies, myEducations, myExperiences, myPortfolio} = resumeData
+
+    for (const {degree, institution} of myEducations) {
+        const eduTile = new HTMLeducation(degree, institution)
+        eduTile.renderToPage();
+    };
+
+    // Code to Add Education
+    // Code to Add Skills
+
+
+    for (const {company, address, position, period, tags, softwares, duties} of myExperiences) {
+        const expTile = new HTMLexpTile(company, address, position, period, tags, softwares, duties)
+        expTile.renderToPage()
+    };
+
+    for (const {projectName, projectLangs, projectTags, summarySmall, summaryLarge, projectUrl, projectImages} of myPortfolio) {
+        const portTile = new HTMLportfolioTile(projectName, projectLangs, projectTags, summarySmall, summaryLarge, projectUrl, projectImages);
+        portTile.renderToPage()
+    };
+});
